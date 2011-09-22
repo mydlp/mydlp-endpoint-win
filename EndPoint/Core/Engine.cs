be8f0a3a09112702300f81b4194598a05db99222
Ozgur Batur <ozgurbtr@gmail.com>
2011-09-22 11:00:56 +0300

    mydlp-engine start env variable problem fixed with short paths

--- EndPoint/Core/Engine.cs
+++ EndPoint/Core/Engine.cs
@@ -21,19 +21,28 @@ using System;
 using System.Collections.Generic;
 using System.Text;
 using System.Threading;
+using System.Runtime.InteropServices;
 
 namespace MyDLP.EndPoint.Core
 {
     public class Engine
     {
+        [DllImport("kernel32.dll", CharSet = CharSet.Auto)]
+        public static extern int GetShortPathName(
+            [MarshalAs(UnmanagedType.LPTStr)]
+        string path,
+            [MarshalAs(UnmanagedType.LPTStr)]
+        StringBuilder shortPath,
+            int shortPathLength
+            );
+
         String pythonStartCmd = @"cd " + Configuration.PyBackendPath + " && Run.bat";        
         String erlStartCmd = @"cd " + Configuration.ErlangPath + " && Run.bat";
 
         public void Start()
         {
             ExecuteCommandAsync(pythonStartCmd);
-            ExecuteCommandAsync(erlStartCmd);
-            RegisterAndStartErlSrv();
+            ExecuteCommandAsync(erlStartCmd);            
         }
 
 
@@ -44,10 +53,11 @@ namespace MyDLP.EndPoint.Core
             KillProcByName("erl");
         }
 
-        public void RegisterAndStartErlSrv()
+        public String GetShortPath(String path)
         {
-            
-        
+            StringBuilder shortPath = new StringBuilder(255);
+            GetShortPathName(path, shortPath, shortPath.Capacity);
+            return shortPath.ToString();
         }
 
         public void KillProcByName(String procname)
@@ -124,14 +134,14 @@ namespace MyDLP.EndPoint.Core
                    
                     if (System.Environment.UserInteractive)
                     {
-                        procStartInfo.EnvironmentVariables.Add("MYDLP_CONF", Configuration.AppPath.Replace(@"\", @"/") + "mydlp-ep.conf");
+                        procStartInfo.EnvironmentVariables.Add("MYDLP_CONF", GetShortPath(Configuration.AppPath).Replace(@"\", @"/") + "mydlp-ep.conf");
                     }
                     else
                     {
-                        procStartInfo.EnvironmentVariables.Add("MYDLP_CONF", Configuration.AppPath.Replace(@"\", @"/") + "mydlp.conf");
+                        procStartInfo.EnvironmentVariables.Add("MYDLP_CONF", GetShortPath(Configuration.AppPath).Replace(@"\", @"/") + "mydlp.conf");
                     }
 
-                    procStartInfo.EnvironmentVariables.Add("MYDLPBEAMDIR", Configuration.ErlangPath);
+                    procStartInfo.EnvironmentVariables.Add("MYDLPBEAMDIR", GetShortPath(Configuration.ErlangPath));
 
                     procStartInfo.EnvironmentVariables["path"] = procStartInfo.EnvironmentVariables["path"] + @";" + Configuration.ErlangBinPaths;
                     Logger.GetInstance().Debug("Environment path:" + procStartInfo.EnvironmentVariables["path"]);
