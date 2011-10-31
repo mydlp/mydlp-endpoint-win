4f1b4d1351ade6e5d8a70073d591860f8530f9c9
Ozgur Batur <ozgurbtr@gmail.com>
2011-10-31 04:26:35 +0200

    added tools, service restart tool

---
+++ EndPoint/Tools/SvcRestart/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.ServiceProcess;
+
+namespace SvcRestart
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string serviceName;
+
+            try
+            {
+                if (args.Length != 1)
+                {
+                    throw new Exception("Invalid argument");
+                }
+
+                serviceName = args[0];
+
+                try
+                {
+                    ServiceController service = new ServiceController(serviceName);
+                    
+                    if (!service.Status.Equals(ServiceControllerStatus.Stopped) 
+                        && !service.Status.Equals(ServiceControllerStatus.StopPending))
+                    {
+                        service.Stop();  
+                    }
+                    service.WaitForStatus(ServiceControllerStatus.Stopped);
+                    service.Start();
+                    service.WaitForStatus(ServiceControllerStatus.Running);
+                }
+
+                catch (Exception e)
+                {
+                    Console.WriteLine(e.Message);
+                    return;
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);               
+                return;
+            }
+        }
+    }
+}
