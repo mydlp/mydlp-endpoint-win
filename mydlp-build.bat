ed1ed8d66ce592ee48f1b0af15b9ad125218c24c
Ozgur Batur <ozgurbtr@gmail.com>
2011-12-14 14:31:43 +0200

    add build  scrit

---
+++ mydlp-build.bat
@@ -0,0 +1,6 @@
+@set PATH=c:\msysgit\msysgit\bin;c:\msysgit\msysgit\mingw\bin;c:\msysgit\msysgit\cmd;%PATH%
+
+cd C:\hudson\workspace\mydlp-endpoint-win\EndPoint
+
+
+devenv /rebuild "Release|Any CPU" MyDLP.EndPoint.sln
