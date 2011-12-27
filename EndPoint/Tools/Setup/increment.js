e6785e9edd786839b2dbd1e8a4824d03f193b4f7
Ozgur Batur <ozgurbtr@gmail.com>
2011-12-27 13:20:09 +0200

    fixed blocking shell execution

--- EndPoint/Tools/Setup/increment.js
+++ EndPoint/Tools/Setup/increment.js
@@ -4,15 +4,12 @@ var pdir = WScript.Arguments.Item(1);
 if (config == "Release")
 {   
   var objShell = WScript.CreateObject("WScript.Shell");
-  objShell.run("cmd /C \"git.exe describe --tags > " + pdir + "gitoutput.txt\"");
-
-  
+  objShell.run("cmd /C \"git.exe describe --tags > " + pdir + "gitoutput.txt\"", 0, true); 
   var fs = new ActiveXObject("Scripting.FileSystemObject");
   var f = fs.GetFile(pdir + "gitoutput.txt");  
   var is = f.OpenAsTextStream(1, 0);    
   var rline = is.ReadLine();
   is.Close();
-  sleep(3000);
   fs.DeleteFile(pdir + "gitoutput.txt");
 
   var versionExp = new RegExp("([0-9]+)\.([0-9]+)\.([0-9]+)");
@@ -39,46 +36,6 @@ if (config == "Release")
   f.WriteLine(productVersion);
   f.Close();
 
-  objShell.run("cmd /C \"git.exe tag " + newVersion + "\"");
-  objShell.run("cmd /C \"git.exe push --tags \"");
+  objShell.run("cmd /C \"git.exe tag " + newVersion + "\"",0 , true);
+  objShell.run("cmd /C \"git.exe push --tags \"", 0, true);
 }
-
-//if (config == "Release")
-/*{      
-    var productVersion= "<?xml version=\"1.0\" encoding=\"utf-8\"?> <Include> <?define ProductVersion=\"0.0.0\"?> </Include>";
-    
-    var shell = WScript.CreateObject("WScript.shell"); 
-    var exec = shell.Exec("%comspec% /K dir");   
-    WScript.StdOut.Write(exec.StdOut.ReadALl());    */
-    //WScript.Echo("del " + pdir + "ProductVersion.wxi");    
-    //shell.run("del " + pdir + "ProductVersion.wxi"); 
-    /*     
-    var patversion = new RegExp("ProductVersion=\"([0-9]+)\.([0-9]+)\.([0-9]+)\"");
-    var version = patversion.exec(rline);
-    var oldversion = version[1] + "." + version[2] + "." + version[3];    
-    var newversion =  version[1] + "." + version[2] + "." + ++version[3]; 
-    
-    rline = rline.replace(oldversion,newversion);
-    WScript.Echo(rline);*/
-    /*
-    f = fs.CreateTextFile(pdir + "ProductVersionNew.wxi");
-    f.WriteLine(rline);
-    f.Close();
-
-    f = fs.GetFile(pdir + "ProductVersion.wxi");
-    f.Delete(true);
-
-    f = fs.GetFile(pdir + "ProductVersionNew.wxi");
-    f.Move(pdir + "ProductVersion.wxi");
-    */
-//}
-
-function sleep(milliseconds) {
-  var start = new Date().getTime();
-  for (var i = 0; i < 1e7; i++) {
-    if ((new Date().getTime() - start) > milliseconds){
-      break;
-    }
-  }
-}
-
