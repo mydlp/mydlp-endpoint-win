b5fa8a406f09303b160dc8f4ac3fa415f58e648c
Ozgur Batur <ozgurbtr@gmail.com>
2012-01-02 15:29:09 +0200

    another small fix

--- EndPoint/Tools/Setup/moveversion.js
+++ EndPoint/Tools/Setup/moveversion.js
@@ -15,6 +15,7 @@ if (config == "Release")
     var patversion = new RegExp("ProductVersion=\"([0-9]+)\.([0-9]+)\.([0-9]+)\"");
     var version = patversion.exec(rline);
     var fullversion = version[1] + "_" + version[2] + "_" + version[3];    
+    var newVersion = version[1] + "." + version[2] + "." + version[3]; 
     
     var objShell = WScript.CreateObject("WScript.Shell");
     objShell.run("cmd /C \"C:\\WinDDK\\7600.16385.1\\bin\\x86\\SignTool.exe sign /sha1 cb2645d541b0f15ece224fc26c8d713711069e9e /t http://timestamp.globalsign.com/scripts/timestamp.dll "  + pdir + "bin\\Release\\mydlp.msi\"",0 , true);
