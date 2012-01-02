9e2bec1de4acde45d33e79653478fd74c5403235
Ozgur Batur <ozgurbtr@gmail.com>
2012-01-02 14:06:08 +0200

    small fix and tag push only on auto build machine

--- EndPoint/Tools/Setup/moveversion.js
+++ EndPoint/Tools/Setup/moveversion.js
@@ -16,12 +16,15 @@ if (config == "Release")
     var version = patversion.exec(rline);
     var fullversion = version[1] + "_" + version[2] + "_" + version[3];    
     
-    
+    var objShell = WScript.CreateObject("WScript.Shell");
     objShell.run("cmd /C \"C:\\WinDDK\7600.16385.1\\bin\x86\SignTool.exe sign /sha1 cb2645d541b0f15ece224fc26c8d713711069e9e /t http://timestamp.globalsign.com/scripts/timestamp.dll bin\\Release\\mydlp.msi\"",0 , true);
 
     f = fs.GetFile(pdir + "bin\\Release\\mydlp.msi");    
     f.Move(pdir + "bin\\Release\\mydlp_" + fullversion + ".msi"); 
     
-    objShell.run("cmd /C \"git.exe tag -a " + newVersion + " -m \"tag for release: " + newVersion + "\"",0 , true);
-    objShell.run("cmd /C \"git.exe push --tags \"", 0, true);
+    if(fs.FileExists(pdir + "..\..\..\..\ftpscript.txt"))
+    {
+      objShell.run("cmd /C \"git.exe tag -a " + newVersion + " -m \"tag for release: " + newVersion + "\"",0 , true);
+      objShell.run("cmd /C \"git.exe push --tags \"", 0, true);
+    }
 }
