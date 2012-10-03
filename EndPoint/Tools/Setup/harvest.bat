21f287f9f02d4c5d9239c235eb37e800017f37e7
H. Kerem Cevahir <hkerem@gmail.com>
2012-10-03 14:48:15 +0300

    Added internal folder

--- EndPoint/Tools/Setup/harvest.bat
+++ EndPoint/Tools/Setup/harvest.bat
@@ -4,3 +4,4 @@ heat dir "..\..\..\..\..\..\mydlp-endpoint-win\EndPoint\Engine\mydlp\src\mydlp" 
 heat dir "..\..\..\..\..\..\mydlp-endpoint-win\EndPoint\Service\printing" -srd -sreg -cg "printing" -ag -dr PRINTINGDIR -template fragment -var var.printingPath -t ../../filter.xsl -o ../../harvested/printing.wxs
 heat dir "..\..\..\..\..\..\mydlp-deployment-env\jre7" -sreg -cg "jre7" -ag -dr INSTALLLOCATION -template fragment -var var.jre7Path -t ../../filter.xsl -o ../../harvested/jre7.wxs
 heat dir "..\..\..\..\..\..\mydlp-deployment-env\cygwin" -sreg -cg "cygwin" -ag -dr INSTALLLOCATION -template fragment -var var.cygwinPath -t ../../filter.xsl -o ../../harvested/cygwin.wxs
+heat dir "..\..\..\..\..\..\mydlp-deployment-env\internal" -sreg -cg "internal" -ag -dr INSTALLLOCATION -template fragment -var var.internalPath -t ../../filter.xsl -o ../../harvested/internal.wxs
