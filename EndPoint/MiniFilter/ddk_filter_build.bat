c480e5f94f8f5e9677dc217e578350bc9971ac7b
Ozgur Batur <ozgur@mydlp.com>
2012-11-02 10:30:32 +0200

    removed unnecessary inf file and related operations in build script

--- EndPoint/MiniFilter/ddk_filter_build.bat
+++ EndPoint/MiniFilter/ddk_filter_build.bat
@@ -1,5 +1,5 @@
 set pwd=%CD%
-cmd /K "C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ chk x86 WXP no_oacr && cd %pwd% && cd src && build -ceZ && cd .. && copy MyDLPMiniFilter.inf src\objchk_wxp_x86\i386\ && exit" 
-cmd /K "C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ chk x64 Win7 no_oacr && cd %pwd% && cd src && build -ceZ && cd .. && copy MyDLPMiniFilter.inf src\objchk_win7_amd64\amd64\ && exit" 
+cmd /K "C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ chk x86 WXP no_oacr && cd %pwd% && cd src && build -ceZ && cd .. && exit" 
+cmd /K "C:\WinDDK\7600.16385.1\bin\setenv.bat C:\WinDDK\7600.16385.1\ chk x64 Win7 no_oacr && cd %pwd% && cd src && build -ceZ && cd .. && exit" 
 
 cmd /K "cd %pwd% && cd "..\..\..\mydlp-development-env\pp" && processfilter.bat %pwd% && exit" 
